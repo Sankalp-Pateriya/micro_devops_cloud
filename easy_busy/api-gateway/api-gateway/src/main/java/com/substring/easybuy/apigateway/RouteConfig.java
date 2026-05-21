4de538c03cdbd1419b3fc92bb834119d27b6b3c8
batchlcwd <batchlcwd@gmail.com>
2026-05-21 23:03:11 +0530

    config server added

--- easy_busy/api-gateway/api-gateway/src/main/java/com/substring/easybuy/apigateway/RouteConfig.java
+++ easy_busy/api-gateway/api-gateway/src/main/java/com/substring/easybuy/apigateway/RouteConfig.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class RouteConfig {
@@ -40,5 +41,7 @@ public class RouteConfig {
 
                 .build();
     }
+
+
 
 }
